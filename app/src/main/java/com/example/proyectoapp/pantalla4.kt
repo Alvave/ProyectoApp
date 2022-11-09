e017371790d6481c80e4c1a30ff09895ac74d33a
Alvave <alejavave1501@gmail.com>
2022-11-09 15:56:52 -0500

    primer cmmit

---
+++ app/src/main/java/com/example/proyectoapp/pantalla4.kt
@@ -0,0 +1,11 @@
+package com.example.proyectoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pantalla4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla4)
+    }
+}
